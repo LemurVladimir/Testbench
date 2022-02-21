d15fb331dbce8e33af3489972d00e6cf896886c0
Vladimir <lemur.vladimir@gmail.com>
2022-02-21 03:23:45 +0300

    Revert "Fixes and additions"
    This reverts commit a1974cc25ec213b66c671a21e58a2136566ffcd5.

--- src/main.cs
+++ src/main.cs
@@ -1,6 +1,6 @@
-﻿using Mono.Options;
-using System;
+﻿using System;
 using System.Diagnostics;
+using Mono.Options;
 
 namespace ProcessStalker {
 	class Loader {
@@ -8,9 +8,9 @@ namespace ProcessStalker {
 		internal static Params settings;
 		private static bool need_help;
 		internal class Params {
-			internal int Time { private set; get; }
+			internal uint Time { private set; get; }
 			internal string Path { private set; get; }
-			internal Params(string path, int time = 5) {
+			internal Params(string path, uint time = 5) {
 				Time = time;
 				Path = path;
 			}
@@ -19,29 +19,17 @@ namespace ProcessStalker {
 		static void Main(string[] args) {
 			Preloader(args);
 		}
-		/// <summary>
-		/// Pre-run: Parses the passed arguments and checks them for validity.
-		/// </summary>
-		/// <param name="args">A string of arguments.</param>
+
 		internal static void Preloader(string[] args) {
-			need_help = false;
 			string _path = ""; // A path to the file to be executed.
-			int _delay = 0; // A delay between each snapshot (in ms).
+			uint _delay = 0; // A delay between each snapshot (in ms).
 			options = new OptionSet() {
 				{ "p|path=", "path to the file to be executed.",
 					_ => _path = _.Replace("/","\\") },
 				{ "d|delay=", "delay (in seconds) between each collection.\nthis must be an integer.",
-					_ => {
-						if (int.TryParse(_,out int x))
-							_delay = x >= 0 ? x * 1000 : x * -1000;
-						else { 
-							_delay = 5000;
-							Console.WriteLine("Couldn't parse the Delay value, proceeding with the default (5 seconds).");
-						}
-					} 
-				},
+					(uint _) => _delay = _ * 1000 },
 				{ "h|help", "show this message and exit",
-					x => need_help = x != null },
+					_ => need_help = _ != null },
 			};
 			try {
 				_ = options.Parse(args);
@@ -57,23 +45,16 @@ namespace ProcessStalker {
 				new Params(path: _path, time: _delay) : 
 				new Params(path: _path);
 			Run();
+			
 		}
 
-		/// <summary>
-		/// Executes all the logic after passing the pre-run tests.
-		/// </summary>
 		internal static void Run() {
-			Console.WriteLine(
-				"\n" +
-				"╔═════════════════════╦═══════════════════╦══════╗\n" +
-				"║ Process Stalker (α) ║ by Vladimir Lemur ║ 2022 ║\n" +
-				"╚═════════════════════╩═══════════════════╩══════╝\n");
+			need_help = false;
 			if (need_help) {
 				Logic.Help(options);
 				return;
 			}
 			else {
-				Console.Write("Starting a victim process: ");
 				using (Process victim = new Process()) {
 					victim.StartInfo.CreateNoWindow = true;
 					victim.StartInfo.FileName = settings.Path;
@@ -81,28 +62,14 @@ namespace ProcessStalker {
 					try {
 						victim.Start();
 					}
-					catch (Exception e) {
-						Console.Write(
-							"Failure!\n" +
-							"\n" +
-							"Exception caught while starting the victim process:\n" +
-							$"{e}\n" +
-							"\n" +
-							"Press ENTER to exit.");
-						Console.Read();
-						Console.WriteLine("\tExiting...");
+					catch {
+						Console.Write("Exception caught starting the victim process.\nPress ENTER to exit.");
+						Console.ReadLine();
+						Console.WriteLine("Exiting...");
 						return;
 					}
-					Console.WriteLine("Done!");
-					try { 
-						Logic.RunStalker(victim, settings.Time); 
-					}
-					catch (Exception e) { 
-						Console.WriteLine($"\nThere was an error during the execution:\n{e}"); 
-					}
-					finally { 
-						if (!victim.HasExited) victim.Kill(); 
-					}
+					Logic.RunStalker(victim, (int)settings.Time);
+					victim.Kill();
 				}
 			}
 		}
