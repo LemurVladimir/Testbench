62cbfebd31ab56fa92d3d93367a87a6ef2037b79
Vladimir <lemur.vladimir@gmail.com>
2022-02-21 03:30:15 +0300

    Release v0.1

---
+++ src/logic.cs
@@ -0,0 +1,47 @@
+﻿using Mono.Options;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace ProcessStalker {
+	internal class Logic {
+		internal static string outputPath; // Somehow for some unknown reason I get the errors up the ass while attempting to remake this as an inner var in RunStalker;
+		internal static void RunStalker(Process process, int delay) {
+			int c = 0;
+			Console.Write("Beginning to stalk a victim. Press any key to stop.\nSnapshots taken: 0");
+			if (!Directory.Exists(".\\Logs"))	Directory.CreateDirectory(".\\Logs");
+			outputPath = string.Format("Logs\\{0}_{1:yyyy}-{1:MM}-{1:dd}_{1:HH}-{1:mm}-{1:ss}.csv", process.ProcessName, DateTime.Now);
+			PerformanceCounter cpuPerfCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName, true);
+			using (var sw = new StreamWriter(outputPath)) {
+				while (!Console.KeyAvailable) {
+					if (process.HasExited) {
+						Console.Write(
+							"\n╔══════════════════════════════════╗" +
+							"\n║ERROR: Victim has been terminated!║" +
+							"\n╚══════════════════════════════════╝");
+						break;
+					} 
+					else {
+						sw.WriteLine($"{cpuPerfCounter.NextValue()},{process.WorkingSet64},{process.PrivateMemorySize64},{process.HandleCount}");
+					}
+					Console.Write($"\rSnapshots taken: {++c}");
+					Thread.Sleep(delay);
+				}
+				Console.WriteLine("\nStalking interrupted, shutting down...");
+				sw.Close();
+			}
+		}
+
+		/// <summary>
+		/// Outputs the help to console when -h or --help is prompted.
+		/// </summary>
+		/// <param name="_">A Mono.Options set to output when -h or --help is prompted.</param>
+		internal static void Help(OptionSet _) {
+			Console.WriteLine("Usage: procstalker [OPTIONS]");
+			Console.WriteLine("Executes a program and logs its CPU and memory usage to a .CSV file.\n");
+			Console.WriteLine("Options:");
+			_.WriteOptionDescriptions(Console.Out);
+		}
+	}
+}
---
+++ src/main.cs
@@ -0,0 +1,112 @@
+﻿using Mono.Options;
+using System;
+using System.Diagnostics;
+
+namespace ProcessStalker {
+	class Loader {
+		private static OptionSet options;
+		internal static Params settings;
+		private static bool need_help;
+		internal class Params {
+			internal int Time { private set; get; }
+			internal string Path { private set; get; }
+			internal Params(string path, int time = 5) {
+				Time = time;
+				Path = path;
+			}
+		}
+
+		static void Main(string[] args) {
+			Preloader(args);
+		}
+		/// <summary>
+		/// Pre-run: Parses the passed arguments and checks them for validity.
+		/// </summary>
+		/// <param name="args">A string of arguments.</param>
+		internal static void Preloader(string[] args) {
+			need_help = false;
+			string _path = ""; // A path to the file to be executed.
+			int _delay = 0; // A delay between each snapshot (in ms).
+			options = new OptionSet() {
+				{ "p|path=", "path to the file to be executed.",
+					_ => _path = _.Replace("/","\\") },
+				{ "d|delay=", "delay (in seconds) between each collection.\nthis must be an integer.",
+					_ => {
+						if (int.TryParse(_,out int x))
+							_delay = x >= 0 ? x * 1000 : x * -1000;
+						else { 
+							_delay = 5000;
+							Console.WriteLine("Couldn't parse the Delay value, proceeding with the default (5 seconds).");
+						}
+					} 
+				},
+				{ "h|help", "show this message and exit",
+					x => need_help = x != null },
+			};
+			try {
+				_ = options.Parse(args);
+			}
+			catch (OptionException e) {
+				Console.Write("procstalker: ");
+				Console.WriteLine(e.Message);
+				Console.WriteLine("Try `procstalker --help' for more information.");
+				return;
+			}
+			settings = 
+				(_delay != 0) ? 
+				new Params(path: _path, time: _delay) : 
+				new Params(path: _path);
+			Run();
+		}
+
+		/// <summary>
+		/// Executes all the logic after passing the pre-run tests.
+		/// </summary>
+		internal static void Run() {
+			Console.WriteLine(
+				"\n" +
+				"╔═════════════════════╦═══════════════════╦══════╗\n" +
+				"║ Process Stalker (α) ║ by Vladimir Lemur ║ 2022 ║\n" +
+				"╚═════════════════════╩═══════════════════╩══════╝\n");
+			if (need_help) {
+				Logic.Help(options);
+				return;
+			}
+			else {
+				Console.Write("Starting a victim process: ");
+				using (Process victim = new Process()) {
+					victim.StartInfo.CreateNoWindow = true;
+					victim.StartInfo.FileName = settings.Path;
+					victim.StartInfo.UseShellExecute = false;
+					try {
+						victim.Start();
+					}
+					catch (Exception e) {
+						Console.Write(
+							"Failure!\n" +
+							"\n" +
+							"Exception caught while starting the victim process:\n" +
+							$"{e}\n" +
+							"\n" +
+							"Press ENTER to exit.");
+						Console.Read();
+						Console.WriteLine("\tExiting...");
+						return;
+					}
+					Console.WriteLine("Done!");
+					try { 
+						Logic.RunStalker(victim, settings.Time); 
+					}
+					catch (Exception e) { 
+						Console.WriteLine($"\nThere was an error during the execution:\n{e}"); 
+					}
+					finally { 
+						if (!victim.HasExited) victim.Kill(); 
+					}
+				}
+			}
+		}
+
+		
+	}
+}
